009a0c7195dd901563c5e348fa7175a01ad1fe01
chittenden-daniel <danielchittenden@yahoo.com>
2022-08-27 15:54:20 -0700

    NavMenu started

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import NavMenu from './components/NavMenu';
 
 function App() {
   return (
+	<BrowserRouter>
     <div className="App">
-		
+		<NavMenu></NavMenu>
+		<h1>scroll testing</h1>
+		<h1>scroll testing</h1>
+		<h1>scroll testing</h1>
+		<h1>scroll testing</h1>
+		<h1>scroll testing</h1>
+		<h1>scroll testing</h1>
+		<h1>scroll testing</h1>
+		<h1>scroll testing</h1>
+		<h1>scroll testing</h1>
+		<h1>scroll testing</h1>
+		<h1>scroll testing</h1>
+		<h1>scroll testing</h1>
+		<h1>scroll testing</h1>
+		<h1>scroll testing</h1>
+		<h1>scroll testing</h1>
+		<h1>scroll testing</h1>
+		<h1>scroll testing</h1>
+		<h1>scroll testing</h1>
+		<h1>scroll testing</h1>
+		<h1>scroll testing</h1>
+		<h1>scroll testing</h1>
+		<h1>scroll testing</h1>
     </div>
+	</BrowserRouter>
   );
 }
 
