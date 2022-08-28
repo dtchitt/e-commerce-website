d32b413033cb20c49c4ea20058c7c34a1c023dac
chittenden-daniel <danielchittenden@yahoo.com>
2022-08-27 18:12:44 -0700

    navbar setup

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,21 +1,53 @@
-import React from 'react'
-import { Container } from 'react-bootstrap';
-import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Image from "react-bootstrap/Image";
+import Navbar from "react-bootstrap/Navbar";
+import { LinkContainer } from "react-router-bootstrap";
 
 const NavMenu: React.FC = () => {
-  return (
-	<Navbar bg='dark' variant='dark' expand='lg' fixed='top' collapseOnSelect>
-		<Container fluid>
-			<Navbar.Brand as={Link} to='/'>e-commerce</Navbar.Brand>
-			<Navbar.Toggle  aria-controls='responsive-navbar-nav'/>
-			<Navbar.Collapse id="responsive-navbar-nav">
+	return (
+		<Navbar
+			bg="dark"
+			variant="dark"
+			expand="lg"
+			collapseOnSelect
+			fixed="top"
+		>
+			<Container fluid>
+				<LinkContainer to="/">
+					<Navbar.Brand className="ms-0">e-commerce</Navbar.Brand>
+				</LinkContainer>
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className="mx-auto">
+						<LinkContainer to="/about">
+							<Nav.Link>About</Nav.Link>
+						</LinkContainer>
+						<LinkContainer to="/about">
+							<Nav.Link>About</Nav.Link>
+						</LinkContainer>
+						<LinkContainer to="/about">
+							<Nav.Link>About</Nav.Link>
+						</LinkContainer>
+					</Nav>
+					<Nav className="ms-0">
+						<LinkContainer to="/login">
+							<Nav.Link>Login</Nav.Link>
+						</LinkContainer>
+						<LinkContainer to="/cart">
+							<Nav.Link>
+								<Image
+									src={require("../media/shopping-cart.png")}
+									fluid
+								/>
+							</Nav.Link>
+						</LinkContainer>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
+};
 
-
-			</Navbar.Collapse>
-		</Container>
-	</Navbar>
-  )
-}
-
-export default NavMenu
+export default NavMenu;
