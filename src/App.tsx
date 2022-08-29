41c85a6dc526e8cbff47e315e1e724250a319d47
chittenden-daniel <danielchittenden@yahoo.com>
2022-08-28 17:07:52 -0700

    created secondaryNav

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import "./App.css";
+import SecondaryNav from "./components/navigation/SecondaryNav";
 
 function App() {
 	return (
 		<div className="App">
 			<PrimaryNav />
+			<SecondaryNav />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/about" element={<About />} />
