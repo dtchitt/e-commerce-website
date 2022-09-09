117648a312abecb1c74b54ad4c9ba8b36a0e0019
Liam McCabe <89570377+lmccabe267@users.noreply.github.com>
2022-09-08 21:21:44 -0700

    conditional on sale tag for price

--- src/components/pages/Product.tsx
+++ src/components/pages/Product.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import Image from 'react-bootstrap/Image'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Badge } from 'react-bootstrap'
 export default function Product() {
+
+    const price = (onSale: boolean) => {
+        if(onSale){
+            return(
+                <h4>$XX.XX <Badge bg="warning">SALE</Badge></h4>
+            )
+        }else{
+            return(
+                <h4>$XX.XX</h4>
+            )
+        }
+    }
 
   return (
     <>
@@ -21,6 +33,16 @@ export default function Product() {
                     <Image className='p-0' src='#' alt='' style={{ width:'50vh', height:'30vh' }}/>
                     <Image className='p-0' src='#' alt='' style={{ width:'50vh', height:'30vh' }}/>
                 </div>
+            </Col>
+        </Row>
+        <Row className='mt-4'>
+            <Col xs={7}>
+                <h3>Product Title</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque aspernatur aut repellat ducimus pariatur molestias voluptatum ad distinctio. Aut magnam laboriosam deserunt labore illo sunt distinctio ipsa neque asperiores enim.</p>
+            </Col>
+            <Col xs={5}>
+                {price(true)}
+                
             </Col>
         </Row>
     </Container>
