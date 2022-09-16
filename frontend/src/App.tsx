ee039f5003365a9035b891ab5ff438a8e6267539
Liam McCabe <89570377+lmccabe267@users.noreply.github.com>
2022-09-15 22:05:38 -0700

    commit before pull from main

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,6 +18,7 @@ function App() {
 					<Route path="/" element={<Home />} />
 					<Route path="/about" element={<About />} />
 					<Route path="/product" element={<Product />} />
+					<Route path="/profile" element={<Profile />} />
 				</Route>
 			</Routes>
 		</div>
