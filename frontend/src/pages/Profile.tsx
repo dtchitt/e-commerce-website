8a0270afddba202be97ed69159135bc0fa153359
Liam McCabe <89570377+lmccabe267@users.noreply.github.com>
2022-09-26 17:05:59 -0700

    added info panels to profile page

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,20 +1,54 @@
 import React from 'react';
-import { Image, Container, Row, Col } from 'react-bootstrap';
+import { Button, Image, Container, Row, Col } from 'react-bootstrap';
 import sampleImage from '../media/profileSample.png';
 
 export default function Profile() {
 	return (
-		<Container>
-			<Row className='w-100 h-100 p-0 m-0'>
-				<Col xs={6}>
+		<Container className='py-4'>
+			<Row>
+				{/* left side: */}
+				<Col xs={6} className='text-center'>
 					<Image
 						src={sampleImage}
 						width={400}
 						height={400}
 						className='shadow border border-dark rounded-circle'
 					/>
+					<Row>
+						<Col xs={10} className='my-3 mx-auto'>
+							<h3 className='shadow rounded'>USERNAME</h3>
+							<h3 className='shadow rounded'>EMAIL</h3>
+							<Button variant='primary' className='shaodow'>
+								Edit Profile
+							</Button>
+						</Col>
+					</Row>
 				</Col>
-				<Col xs={6}></Col>
+				{/* Right Side */}
+				<Col className='p-3 bg-secondary rounded'>
+					<Row className='d-flex justify-content-around my-3' style={{ height: '45%' }}>
+						<Col xs={5} className='bg-light rounded shadow' style={{ width: '45%' }}>
+							hello
+						</Col>
+						<Col xs={5} className='bg-light rounded shadow' style={{ width: '45%' }}>
+							hello
+						</Col>
+					</Row>
+					<Row className='d-flex justify-content-around my-3' style={{ height: '45%' }}>
+						<Col xs={5} className='bg-light rounded shadow' style={{ width: '45%' }}>
+							<Col xs={10} className='my-3 text-center mx-auto'>
+								<h3 className='shadow'>SHIPPING ADDRESS</h3>
+								<h3 className='shadow'>PAYMENT METHOD</h3>
+								<Button variant='primary' className='shaodow'>
+									EDIT BILLING/SHIPPING
+								</Button>
+							</Col>
+						</Col>
+						<Col xs={5} className='bg-light rounded shadow' style={{ width: '45%' }}>
+							hello
+						</Col>
+					</Row>
+				</Col>
 			</Row>
 		</Container>
 	);
