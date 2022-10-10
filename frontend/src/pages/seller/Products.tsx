63b3f2512f3cb67741befa0a37454f2e52f831cc
Liam McCabe <89570377+lmccabe267@users.noreply.github.com>
2022-10-10 00:53:28 -0700

    changed planned dashboard to product page

--- frontend/src/pages/seller/Products.tsx
+++ frontend/src/pages/seller/Products.tsx
@@ -1,5 +1,29 @@
-import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import './styles/cms.css';
 
-export default function Products() {
-	return <div>products</div>;
-}
+const Product = () => {
+	return (
+		<Container fluid className=''>
+			<Row>
+				<Col xs={3} className='px-5 py-4 d-flex justify-content-around'>
+					<Button className='px-4'>NEW</Button>
+					<Button className='px-4'>SORT</Button>
+				</Col>
+				<Col></Col>
+				<Col xs={3} className='px-5 py-4 d-flex justify-content-around'>
+					<Button className='px-4'>EDIT</Button>
+					<Button variant='danger' className='px-4'>
+						DELETE
+					</Button>
+				</Col>
+			</Row>
+			<Row className='d-flex justify-content-around'>
+				<Col xs={8} className='bg-white shadow mt-2 rounded' style={{ minHeight: '70vh' }}></Col>
+				<Col xs={3} className='bg-white shadow mt-2 rounded' style={{ minHeight: '70vh' }}></Col>
+			</Row>
+		</Container>
+	);
+};
+
+export default Product;
