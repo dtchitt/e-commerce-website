67f8a61510e041bb0944d9e302434f6e37fd2d8a
chittenden-daniel <danielchittenden@yahoo.com>
2022-10-10 22:34:38 -0700

    update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,22 +7,25 @@ import { Login } from './pages/Login';
 import Profile from './pages/Profile';
 import Cart from './pages/Cart';
 import './styles/general.css';
+import { CartProvider } from './context/CartContext';
 
 function App() {
 	return (
-		<div id ='root-div'>
-			<Routes>
-				<Route element={<Outlet />}>
-					<Route path='/login' element={<Login />} />
-				</Route>
-				<Route element={<Navigation />}>
-					<Route path='/' element={<Home />} />
-					<Route path='/cart' element={<Cart />} />
-					<Route path='/about' element={<About />} />
-					<Route path='/product' element={<Product />} />
-					<Route path='/profile' element={<Profile />} />
-				</Route>
-			</Routes>
+		<div id='root-div'>
+			<CartProvider>
+				<Routes>
+					<Route element={<Outlet />}>
+						<Route path='/login' element={<Login />} />
+					</Route>
+					<Route element={<Navigation />}>
+						<Route path='/' element={<Home />} />
+						<Route path='/cart' element={<Cart />} />
+						<Route path='/about' element={<About />} />
+						<Route path='/product' element={<Product />} />
+						<Route path='/profile' element={<Profile />} />
+					</Route>
+				</Routes>
+			</CartProvider>
 		</div>
 	);
 }
