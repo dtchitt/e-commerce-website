65477938c5290400013fd1d125d75363794eb030
Liam McCabe <89570377+lmccabe267@users.noreply.github.com>
2022-10-11 22:08:09 -0700

    created analyitcs basic layout

--- frontend/src/pages/seller/Analytics.tsx
+++ frontend/src/pages/seller/Analytics.tsx
@@ -1,5 +1,25 @@
-import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 export default function Analytics() {
-	return <div>analytics</div>;
+	return (
+		<Container fluid>
+			<Row className='d-flex justify-content-around px-3 pt-4'>
+				<Col xs={8}>
+					<Row className='p-0 m-0'>
+						<Col className='bg-white rounded me-1 shadow' style={{ minHeight: '50vh' }}>
+							graphs
+						</Col>
+					</Row>
+					<Row className='p-0 m-0'>
+						<Col className='bg-white mt-4 rounded me-1 shadow' style={{ minHeight: '30vh' }}>
+							graph options
+						</Col>
+					</Row>
+				</Col>
+				<Col xs={4} className='bg-white rounded shadow'>
+					graph insights
+				</Col>
+			</Row>
+		</Container>
+	);
 }
