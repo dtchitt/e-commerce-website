3731b0c43da5723da567115cc11b7300a82d3db9
chittenden-daniel <danielchittenden@yahoo.com>
2022-10-22 10:27:05 -0700

    sequelize cli testing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,11 @@
 import express from 'express';
-import { sequelize as db } from './database';
+import db from './models';
 
 const app = express();
 const port = 3001;
 
-app.listen(port, async () => {
-	console.log(`Express is listening at http://localhost:${port}`);
-
-	try {
-		await db.authenticate();
-		console.log('Connection has been established successfully.');
-	} catch (error) {
-		console.error('Unable to connect to the database:', error);
-	}
+db.sequelize.sync().then(() => {
+	app.listen(port, () => {
+		console.log(`App listening on port ${port}`);
+	});
 });
