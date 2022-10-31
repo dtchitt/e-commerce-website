90c9332d937a06668c0782b5c4cb75d160b61059
Liam McCabe <89570377+lmccabe267@users.noreply.github.com>
2022-10-30 20:49:47 -0700

    basic seed completed

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -7,10 +7,11 @@ const prisma = new PrismaClient();
 
 async function main() {
 	console.log('seeding');
-	const john = await prisma.users.upsert({
-		where: { username: 'jsmith5' },
+	const user1 = await prisma.users.upsert({
+		where: { user_id: 1 },
 		update: {},
 		create: {
+			user_id: 1,
 			username: 'jsmith5',
 			first_name: 'John',
 			last_name: 'Smith',
@@ -18,33 +19,131 @@ async function main() {
 			password: 'password',
 		},
 	});
-	const seededUserID = await prisma.users
-		.findMany({
-			where: { username: 'jsmith5' },
-		})
-		.then((record: any | null) => {
-			if (record) return record[0].user_id;
-			// return record.user_id;
-		});
-	console.log(seededUserID);
+	const user2 = await prisma.users.upsert({
+		where: { user_id: 2 },
+		update: {},
+		create: {
+			user_id: 2,
+			username: 'egomez123',
+			first_name: 'Elijah',
+			last_name: 'Gomez',
+			email: 'email2@email.com',
+			password: 'password',
+		},
+	});
 	const address1 = await prisma.addresses.upsert({
 		where: {
-			user_id: seededUserID,
+			address_id: 1,
 		},
 		update: {},
 		create: {
+			address_id: 1,
 			street: '1234 street rd',
 			city: 'new york',
 			state: 'new york',
 			zipcode: 12345,
 			instructions: 'knock on the front door',
-			users: {
-				connect: { username: 'jsmith5' },
-			},
+			user_id: 1,
+		},
+	});
+	const cart1 = await prisma.carts.upsert({
+		where: {
+			cart_id: 1,
+		},
+		update: {},
+		create: {
+			cart_id: 1,
+			user_id: 1,
 		},
 	});
 
-	console.log(john);
+	const product1 = await prisma.products.upsert({
+		where: { product_id: 1 },
+		update: {},
+		create: {
+			product_id: 1,
+			image_url: 'imageurl',
+			description: 'a very fun chewy toy for a dog',
+			price: 9.99,
+			stock: 25,
+			name: 'Dog Chew Toy',
+		},
+	});
+
+	const cartItem1 = await prisma.cart_items.upsert({
+		where: { cart_item_id: 1 },
+		update: {},
+		create: {
+			cart_item_id: 1,
+			quantity: 2,
+			product_id: 1,
+			cart_id: 1,
+		},
+	});
+
+	const orderDate = new Date(2022, 10, 27, 5, 45, 0, 0);
+	const order1 = await prisma.orders.upsert({
+		where: {
+			order_id: 1,
+		},
+		update: {},
+		create: {
+			order_id: 1,
+			status: 'pending',
+			date: orderDate,
+			time: orderDate,
+			user_id: 1,
+		},
+	});
+
+	const product2 = await prisma.products.upsert({
+		where: { product_id: 2 },
+		update: {},
+		create: {
+			product_id: 2,
+			image_url: 'imageurl',
+			description: 'a very fun scooter for a kid',
+			price: 39.99,
+			stock: 20,
+			name: 'Scooter',
+		},
+	});
+
+	const orderItem1 = await prisma.order_items.upsert({
+		where: {
+			order_item_id: 1,
+		},
+		update: {},
+		create: {
+			order_item_id: 1,
+			quantity: 3,
+			product_id: 2,
+			order_id: 1,
+		},
+	});
+
+	const review1 = await prisma.reviews.upsert({
+		where: { review_id: 1 },
+		update: {},
+		create: {
+			review_id: 1,
+			rating: 0.5,
+			description: 'my dog really liked it',
+			user_id: 2,
+			product_id: 1,
+		},
+	});
+	const review2 = await prisma.reviews.upsert({
+		where: { review_id: 2 },
+		update: {},
+		create: {
+			review_id: 2,
+			rating: 0.7,
+			description: 'my kid really liked it',
+			user_id: 2,
+			product_id: 2,
+		},
+	});
 }
 
 main()
