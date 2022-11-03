8876fed6bd6b6bae725f042c7704535b316691a8
John Salinas <46495232+JohnSalinas123@users.noreply.github.com>
2022-11-02 18:24:04 -0700

    fixed endpoint name and import

--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import accountRouter from './src/routes/accountRoutes';
 
 const app = express();
 const port: number = 3001;
@@ -6,7 +7,7 @@ const port: number = 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/login/', require('./routes/loginRoutes'));
+app.use('/api/user', accountRouter);
 
 app.listen(port, async () => {
 	console.log(`Express is listening at http://localhost:${port}`);
