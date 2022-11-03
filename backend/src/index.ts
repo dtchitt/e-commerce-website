c1566a07ae072ec264805217e6ff252dd14d01c2
John Salinas <46495232+JohnSalinas123@users.noreply.github.com>
2022-11-02 17:47:16 -0700

    moved index.ts file

--- backend/src/index.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-const app = express();
-const port: number = 3001;
-
-app.listen(port, async () => {
-	console.log(`Express is listening at http://localhost:${port}`);
-});
